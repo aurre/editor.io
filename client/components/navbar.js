cda511a79bd7a1837ee37507319b77941e6376aa
raysa <aurrecochearaysa@gmail.com>
2018-10-22 14:13:59 -0500

    edit readme

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
+    <h1>editor.io</h1>
     <nav>
       {isLoggedIn ? (
         <div>
@@ -24,7 +24,6 @@ const Navbar = ({handleClick, isLoggedIn}) => (
         </div>
       )}
     </nav>
-    <hr />
   </div>
 )
 
